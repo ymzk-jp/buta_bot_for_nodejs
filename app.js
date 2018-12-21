5bcd7aa440ca3f51cb589cb9d56ac94bebc02aff
ymzk-jp <ymzk@gmail.com>
2018-12-21 23:53:50 +0900

    startable

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
+// discord.js モジュールのインポート
 const Discord = require('discord.js');
+
+// Discord Clientのインスタンス作成
 const client = new Discord.Client();
+require('dotenv').config();
 
+// トークンの用意
+// const token = process.env.TOKEN;
+const token = process.env.TOKEN;
 
+// 準備完了イベントのconsole.logで通知黒い画面に出る。
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log('ready...');
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
 
-client.login('token');
+client.on("message",message => {
+    if(message.author.bot){
+        return;
+    }else{
+        let msg = message.content;
+        let channel = message.channel;
+        let author = message.author.username;
+        message.reply(msg)
+            .then(message => console.log(`Sent message: ${msg}`))
+            .catch(console.error);
+        return;
+   }
+}
+);
+
+// Discordへの接続
+client.login(token);
